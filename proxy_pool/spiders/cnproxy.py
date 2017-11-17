48edf733208d02114862dd612d7ad3fb35d9b402
anson2416 <ww123ww@126.com>
2017-11-17 20:49:20 +0800

    Added CN-PROXY
    newly added cnproxy.py

---
+++ proxy_pool/spiders/cnproxy.py
@@ -0,0 +1,23 @@
+# -*- coding: utf-8 -*-
+import scrapy
+import re
+from proxy_pool.items import ProxyPoolItem
+
+
+class XicidailiSpider(scrapy.Spider):
+    name = 'cnproxy'
+    allowed_domains = ['cn-proxy.com']
+    start_urls = ['http://cn-proxy.com/']
+
+    def parse(self, response):
+        ips = re.findall('<td>(\d+\.\d+\.\d+\.\d+)</td>', response.text)
+        ports = re.findall('<td>(\d+)</td>', response.text)
+        types = re.findall('<td class="country">([^<]+)</td>', response.text)
+        protocols = re.findall('<td>(HTTPS?)</td>', response.text)
+        for ip, port, _type, protocol in zip(ips, ports, types, protocols):
+            yield ProxyPoolItem({
+                'ip': ip,
+                'protocol': protocol,
+                'port': port,
+                'types': _type
+            })
